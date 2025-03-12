dc887ef433d2fe30ad76659003379231a12804c3
Yanjun Zhao <yanjunzhao81@gmail.com>
2025-03-12 14:46:41 -0400

    added inversion

--- pwm-demo.c
+++ pwm-demo.c
@@ -94,6 +94,9 @@ int main() {
     pwm_set_wrap(slice_num, WRAPVAL) ;
     pwm_set_clkdiv(slice_num, CLKDIV) ;
 
+    //invert
+    pwm_set_output_polarity (slice_num, 0, 1);
+
     // This sets duty cycle
     pwm_set_chan_level(slice_num, PWM_CHAN_A, 3125);
 
